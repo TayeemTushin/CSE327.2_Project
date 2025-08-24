2301e54bbf0560986644e5fabd95fe6753683512
Tayeem Tushin <tayeem.tushin@northsouth.edu>
2025-08-24 22:37:25 +0600

    Delete RidePartner.php
    d

--- RidePartner.php
+++
@@ -1,8 +0,0 @@
-<?php
-interface RidePartner {
-    public function getDetails();
-    public function getVehicleType();
-    public function getCapacity();
-    public function getEstimatedFare($distance = 5);
-}
-?>
