f1e65b4b39b3c5e5af0f9a216bc213ec4f5219fa
Tayeem Tushin <tayeem.tushin@northsouth.edu>
2025-08-24 22:38:15 +0600

    Delete ShortestRoute.php
    d

--- ShortestRoute.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'RouteStrategyInterface.php';
-class ShortestRoute implements RouteStrategyInterface {
-    public function getRoute(string $start, string $end): string {
-        return "📏 Shortest route from $start to $end via local roads.";
-    }
-}
-?>
