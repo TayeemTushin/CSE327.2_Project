88eaec0817f075790b69c4f295f6d4553cd0b120
Tayeem Tushin <tayeem.tushin@northsouth.edu>
2025-08-24 22:30:18 +0600

    Delete AverageRating.php
    d

--- AverageRating.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'RatingStrategyInterface.php';
-class AverageRating implements RatingStrategyInterface {
-    public function calculate(array $ratings): float {
-        return array_sum($ratings) / count($ratings);
-    }
-}
-?>
