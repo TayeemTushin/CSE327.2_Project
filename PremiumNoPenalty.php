a1063658a4c081dfb818dd6abd4f7e95f50d4043
Tayeem Tushin <tayeem.tushin@northsouth.edu>
2025-08-24 22:36:50 +0600

    Delete PremiumNoPenalty.php
    d

--- PremiumNoPenalty.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'CancellationStrategyInterface.php';
-class PremiumNoPenalty implements CancellationStrategyInterface {
-    public function getCancellationMessage(int $minutesBeforeStart): string {
-        return "🌟 Premium user: Ride cancelled successfully. No charges.";
-    }
-}
-?>
