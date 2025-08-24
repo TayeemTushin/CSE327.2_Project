f5582a78ad4d76a495a94c7e0f2ab8d12d661557
Tayeem Tushin <tayeem.tushin@northsouth.edu>
2025-08-24 22:38:05 +0600

    Delete SharedRideFare.php
    d

--- SharedRideFare.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'FareStrategyInterface.php';
-class SharedRideFare implements FareStrategyInterface {
-    public function calculateFare(float $distance): float {
-        return 2 + ($distance * 6);
-    }
-}
-?>
