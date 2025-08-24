257b66eb565022aafc27a9776c07afbfe848640c
Tayeem Tushin <tayeem.tushin@northsouth.edu>
2025-08-24 22:38:25 +0600

    Delete StandardFare.php
    d

--- StandardFare.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'FareStrategyInterface.php';
-class StandardFare implements FareStrategyInterface {
-    public function calculateFare(float $distance): float {
-        return 5 + ($distance * 10);
-    }
-}
-?>
