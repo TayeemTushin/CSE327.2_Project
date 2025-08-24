4adcb8cd7b9aa3169427c6175220450e6d96d8a8
Tayeem Tushin <tayeem.tushin@northsouth.edu>
2025-08-24 22:33:28 +0600

    Delete FastestRoute.php
    d

--- FastestRoute.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'RouteStrategyInterface.php';
-class FastestRoute implements RouteStrategyInterface {
-    public function getRoute(string $start, string $end): string {
-        return "🚀 Fastest route from $start to $end via highway.";
-    }
-}
-?>
